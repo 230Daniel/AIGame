253fb7d2027a4c13db51a2153530a3749cbe2cf5
Daniel Baynton <daniel.baynton@hotmail.com>
2021-10-23 10:32:05 +0100

    Initial commit

---
+++ src/AIGame/AIGame.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AIGame.Test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
